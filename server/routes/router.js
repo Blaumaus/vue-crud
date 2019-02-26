427af33a0b00f16fab8e1daa0cf1916fb9c425da
ookldev <margareq@protonmail.ch>
2019-02-27 00:45:01 +0200

    Added mail inform

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,7 +1,9 @@
 // Imports
 const express = require('express')
+const nodemailer = require('nodemailer')
 const router = express.Router()
 const Record = require('../models/Record')
+require('dotenv').config()
 
 router.get('/', async (req, res) => {
     res.json(await Record.find())
@@ -14,6 +16,43 @@ router.get(':id', async (req, res) => {
 router.post('/', async (req, res) => {
     const record = new Record(req.body)
     await record.save()
+
+    const output = `
+    <p>You have a new message from vue-study</p>
+    <ul>
+        <li>name: ${req.body.name}</li>
+        <li>email: ${req.body.email}</li>
+        <li>address: ${req.body.address}</li>
+        <li>gender: ${req.body.gender}</li>
+    </ul>
+    `
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.MAIL_LOGIN,
+            pass: process.env.MAIL_PASSWORD
+        }
+    })
+
+    let mailOptions = {
+        from: 'vue app',
+        to: 'margareq@protonmail.ch',
+        subject: 'new user blablabla',
+        text: req.body.name,
+        html: output
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) return console.log(`[ERROR] ${err}`)
+
+        console.log(`Message sent: ${info.messageId}`)
+        console.log(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`)
+
+    })
+
     res.json({ state: "success" })
 })
 
